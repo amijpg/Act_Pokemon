ceb40b9f30dad819b8cad5e5c8922331c14f4738
Eymi O.R <eymi.or@gmail.com>
2026-04-14 12:06:29 -0400

    Menú de productos

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+os.system("clear")
+
+# “Tienda Pokémon”
+# Eres el encargado de desarrollar un programa para una tienda Pokémon llamada “PokéMarket”. En esta tienda, los entrenadores pueden comprar distintos objetos para sus aventuras.
+# Objetivo
+# Crear un programa en Python que permita simular el proceso de compra de un entrenador Pokémon, aplicando descuentos según ciertas condiciones y validando correctamente los datos ingresados.
+ 
+# Requerimientos
+# 1.	Mostrar menú de productos: 
+# El programa debe mostrar el siguiente menú:
+# 1. Pokébola → $1000
+# 2. Poción → $1500
+# 3. Revivir → $3000
+# 4. Baya → $500
+# 5. Finalizar compra
+
+print("Tienda Pokemon ⚈ ․̫ ⚈\n" "Productos:\n" " 1. Pokébola → $1000\n" "2. Poción → $1500\n" " 3. Revivir → $3000\n" " 4. Baya → $500\n" " 5. Finalizar compra\n")
